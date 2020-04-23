d41c77ba5bc6cd98ddd7ce67f8ef2dc9f27bbef1
PaulSpuzello <paspuzello@dmacc.edu>
2020-04-22 20:34:33 -0500

    Finished order bys

--- src/main/java/dmacc/controller/WebController.java
+++ src/main/java/dmacc/controller/WebController.java
@@ -38,6 +38,86 @@ public class WebController {
 		return "results";
 	}
 	
+	@GetMapping("/viewByRating")
+	public String viewAllByRating(Model model) {
+		if(repo.findAll().isEmpty()) {
+			return addNewRestaurant(model);
+		}
+		
+		model.addAttribute("restaurants", repo.findByRating());
+		return "results";
+	}
+	
+	@GetMapping("/viewByType")
+	public String viewAllByType(Model model) {
+		if(repo.findAll().isEmpty()) {
+			return addNewRestaurant(model);
+		}
+		
+		model.addAttribute("restaurants", repo.findByType());
+		return "results";
+	}
+	
+	@GetMapping("/viewByPartySize")
+	public String viewAllByPartySize(Model model) {
+		if(repo.findAll().isEmpty()) {
+			return addNewRestaurant(model);
+		}
+		
+		model.addAttribute("restaurants", repo.findByPartySize());
+		return "results";
+	}
+	
+	@GetMapping("/viewByDistance")
+	public String viewAllByDistance(Model model) {
+		if(repo.findAll().isEmpty()) {
+			return addNewRestaurant(model);
+		}
+		
+		model.addAttribute("restaurants", repo.findByDistance());
+		return "results";
+	}
+	
+	@GetMapping("/viewByKidsMenu")
+	public String viewAllByKidsMenu(Model model) {
+		if(repo.findAll().isEmpty()) {
+			return addNewRestaurant(model);
+		}
+		
+		model.addAttribute("restaurants", repo.findByKidsMenu());
+		return "results";
+	}
+	
+	@GetMapping("/viewByStreet")
+	public String viewAllByStreet(Model model) {
+		if(repo.findAll().isEmpty()) {
+			return addNewRestaurant(model);
+		}
+		
+		model.addAttribute("restaurants", repo.findByStreet());
+		return "results";
+	}
+	
+	@GetMapping("/viewByCity")
+	public String viewAllByCity(Model model) {
+		if(repo.findAll().isEmpty()) {
+			return addNewRestaurant(model);
+		}
+		
+		model.addAttribute("restaurants", repo.findByCity());
+		return "results";
+	}
+	
+	@GetMapping("/viewByState")
+	public String viewAllByState(Model model) {
+		if(repo.findAll().isEmpty()) {
+			return addNewRestaurant(model);
+		}
+		
+		model.addAttribute("restaurants", repo.findByState());
+		return "results";
+	}
+	
 	@GetMapping("/inputRestaurant")
 	public String addNewRestaurant(Model model) {
 		Restaurant r = new Restaurant();
--- src/main/java/dmacc/repository/RestaurantRepository.java
+++ src/main/java/dmacc/repository/RestaurantRepository.java
@@ -13,10 +13,42 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 	
 	public static final String FIND_NAME = "SELECT * FROM restaurant ORDER BY name ASC";
 	public static final String FIND_PRICE = "SELECT * FROM restaurant ORDER BY avg_price ASC";
+	public static final String FIND_RATING = "SELECT * FROM restaurant ORDER BY rating DESC";
+	public static final String FIND_TYPE = "SELECT * FROM restaurant ORDER BY type ASC";
+	public static final String FIND_PARTY_SIZE = "SELECT * FROM restaurant ORDER BY party_size DESC";
+	public static final String FIND_DISTANCE = "SELECT * FROM restaurant ORDER BY distance ASC";
+	public static final String FIND_KIDS_MENU = "SELECT * FROM restaurant ORDER BY has_kids_menu ASC";
+	public static final String FIND_STREET = "SELECT * FROM restaurant ORDER BY street ASC";
+	public static final String FIND_CITY = "SELECT * FROM restaurant ORDER BY city ASC";
+	public static final String FIND_STATE = "SELECT * FROM restaurant ORDER BY state ASC";
 	
 	@Query(value = FIND_NAME, nativeQuery = true)
 	List<Restaurant> findByName();
 	
 	@Query(value = FIND_PRICE, nativeQuery = true)
 	List<Restaurant> findByPrice();
+	
+	@Query(value = FIND_RATING, nativeQuery = true)
+	List<Restaurant> findByRating();
+	
+	@Query(value = FIND_TYPE, nativeQuery = true)
+	List<Restaurant> findByType();
+	
+	@Query(value = FIND_PARTY_SIZE, nativeQuery = true)
+	List<Restaurant> findByPartySize();
+	
+	@Query(value = FIND_DISTANCE, nativeQuery = true)
+	List<Restaurant> findByDistance();
+	
+	@Query(value = FIND_KIDS_MENU, nativeQuery = true)
+	List<Restaurant> findByKidsMenu();
+	
+	@Query(value = FIND_STREET, nativeQuery = true)
+	List<Restaurant> findByStreet();
+	
+	@Query(value = FIND_CITY, nativeQuery = true)
+	List<Restaurant> findByCity();
+	
+	@Query(value = FIND_STATE, nativeQuery = true)
+	List<Restaurant> findByState();
 }
